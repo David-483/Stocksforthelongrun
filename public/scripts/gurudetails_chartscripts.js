559b544e7ea2bbeb26f7185397e057ae178634a2
davidbregulla <david-bregulla@web.de>
2021-12-17 17:29:02 +0100

    Stand 1712

--- public/scripts/gurudetails_chartscripts.js
+++ public/scripts/gurudetails_chartscripts.js
@@ -1,2 +1,4 @@
 var aumchart = new ApexCharts(document.querySelector("#aumchart"), options);
 aumchart.render();
+
+new ApexCharts(document.querySelector("#guruSectorChart"), guruSectorChart).render();
--- routes/gurus.js
+++ routes/gurus.js
@@ -4,35 +4,8 @@ const Guru = require('../models/guru')
 
 router.get('/', async (req, res) => {
     try {
-      var totalvalues = [];
-      var totalholdings = [];
-      var categories = [];
-    const gurus = await Guru.find({})
-
-    for(i=0; i<= Object.keys(gurus[0].daten).length -1; i++){
-      var JAHR = Object.keys(gurus[0].daten)[i];
-      for(j=0; j<= Object.keys(gurus[0].daten[JAHR]).length -1; j++){
-        var QUARTAL = Object.keys(gurus[0].daten[JAHR])[j];
-        console.log(JAHR);
-        console.log(QUARTAL);
-        var totalvaluesWert = gurus[0].daten[JAHR][QUARTAL].totalValue;
-        var totalHoldingsWert = gurus[0].daten[JAHR][QUARTAL].totalHoldings;
-        totalvalues.push(totalvaluesWert);
-        totalholdings.push(totalHoldingsWert);
-      }
-    }
-
-    // for(z = 0; z <= Object.keys(kurs_total.data).length -1; z++){
-    //   //console.log(kurs_total.data);
-    //   KURS_KOMPLETT.push([kurs_total.data[z].date, kurs_total.data[z].adjusted_close]);
-    // }
-
-    var chartData = {"totalvalues" : totalvalues, "totalholdings" : totalholdings};
-    var fullData = {gurus, "chartData": chartData};
-
-    res.json(({Guru: fullData}));
-
-    //res.render('gurus/index', {Guru: gurus})
+      const gurus = await Guru.find({})
+    res.render('gurus/index', {Guru: gurus})
     
     } catch (err){
       res.status(500).json({ message: err.message })
@@ -45,10 +18,8 @@ router.get('/:guru', async (req, res) =>{
       var totalHoldings = [];
       var categories = [];
     const gurus = await Guru.find({})
-
     for(i=0; i<= Object.keys(gurus[0].daten).length -1; i++){
       var JAHR = Object.keys(gurus[0].daten)[i];
-      
       for(j=0; j<= Object.keys(gurus[0].daten[JAHR]).length -1; j++){
         var QUARTAL = Object.keys(gurus[0].daten[JAHR])[j];
         var totalValuesWert = gurus[0].daten[JAHR][QUARTAL].totalValue;
@@ -56,18 +27,50 @@ router.get('/:guru', async (req, res) =>{
         categories.push(JAHR);
         totalValues.push(totalValuesWert);
         totalHoldings.push(totalHoldingsWert);
-  
-      }
-            
+      }       
     }
+
+    var sectors = [];
+    var sectorValues = [];
+
+    for(j=0; j<= Object.keys(gurus[0].daten[JAHR][QUARTAL].infoTable).length -1; j++){
+      var sectorEinheit = gurus[0].daten[JAHR][QUARTAL].infoTable[j].sector;
+      var sectorValue = parseInt(gurus[0].daten[JAHR][QUARTAL].infoTable[j].value);
+      // console.log(sectorEinheit);
+      // console.log(sectorValue);
+      if(j == 0){
+        sectors.push(sectorEinheit);
+        sectorValues.push(sectorValue);
+      }
+      else{
+        if(sectors.includes(sectorEinheit)==true){
+          for(k=0; k <= sectors.length; k++){
+            if(sectors[k] == sectorEinheit){
+                sectorValues[k] = sectorValues[k] + sectorValue;
+            }
+          }
+        } else{
+          sectors.push(sectorEinheit);
+          sectorValues.push(sectorValue);
+        }
+      }
+    }
+
+    var sectorChartData = {"sectorValues" : sectorValues, "categories" : sectors};
+
+    console.log(sectors);
+    console.log(sectorValues);
+
+    
+
 
     // for(z = 0; z <= Object.keys(kurs_total.data).length -1; z++){
     //   //console.log(kurs_total.data);
     //   KURS_KOMPLETT.push([kurs_total.data[z].date, kurs_total.data[z].adjusted_close]);
     // }
 
-    var chartData = {"totalValues" : totalValues, "totalHoldings" : totalHoldings, "categories": categories};
-    var fullData = {gurus, "chartData": chartData};
+    var HoldingChartData = {"totalValues" : totalValues, "totalHoldings" : totalHoldings, "categories": categories};
+    var fullData = {gurus, "HoldingChartData": HoldingChartData, "sectorChartData": sectorChartData};
 
     res.render('gurus/detail/index', {Guru: fullData})
 } catch (err) {
